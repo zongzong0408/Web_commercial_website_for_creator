41f83bb68bc08e2011bf3609dab691a28534cafe
zongzong0408 <zongzongchu0408@gmail.com>
2023-12-26 22:01:14 +0800

    2023/12/26 22:01

---
+++ auto-git.py
@@ -0,0 +1,29 @@
+import os
+from datetime import datetime
+
+def git_auto_commit_push(repo_path, commit_message):
+
+    print(f"\nsystem INFO:\t start git process.\n")
+
+    try:
+
+        os.chdir(repo_path)
+
+        os.system('git add .')
+
+        os.system(f'git commit -m "{commit_message}"')
+
+        os.system('git push origin main')
+
+        print("\nsystem OK:\t git commit and push successful!")
+
+    except Exception as e:
+
+        print(f"system DETAIL:\t {e}")
+
+git_repo_path = "D:\Code\GitHub\zongzongchu0408@gmail.com\Web_commercial_website_for_creator"
+
+current_time = datetime.now()
+commit_message = formatted_time = current_time.strftime("%Y/%m/%d %H:%M")
+
+git_auto_commit_push(git_repo_path, commit_message)
