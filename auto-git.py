974b407eff85eac173e417865c2c5113c425421d
zongzong0408 <zongzongchu0408@gmail.com>
2024-01-15 21:44:46 +0800

    2024/01/15 21:43

--- auto-git.py
+++ auto-git.py
@@ -9,25 +9,21 @@ def git_auto_commit_push(repo_path, commit_message):
     try:
 
         os.chdir(repo_path)
-
         os.system('git add .')
-
         os.system(f'git commit -m "{commit_message}"')
-
         os.system('git push origin main')
-
         print("\nsystem OK:\t git commit and push successful!")
 
     except Exception as e:
 
         print(f"system DETAIL:\t {e}")
 
-git_repo_path = "D:\Code\Web_commercial_website_for_creator"
+git_repo_path = "D:\Code\GitHub\Web_commercial_website_for_creator"
 
 current_time = datetime.now()
 commit_message = formatted_time = current_time.strftime("%Y/%m/%d %H:%M")
 
 while (True):
 
-    time.sleep(30)
+    time.sleep(60)
     git_auto_commit_push(git_repo_path, commit_message)
